e789ab9c31948d92ba7e1c36318890d78fd01b33
ChaseWelsh <fseusb@gmail.com>
2022-09-20 10:34:09 -0600

    commit tree 1f4bfbb7ba2e2692d1e2edfed3be4afba6c8b889

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.select { |test| test.chars.sort == word.chars.sort }
+    end
+end
+
+listen = Anagram.new("listen")
+pp listen.match(%w[enlists google inlets banana])
